6f240c5cce6b89f91bf0ff546b0a184f9402245b
Chef Cheems <chefcheems@protonmail.com>
2021-05-31 16:28:30 +0300

    chore: Disruption simulation

--- packages/pancake-uikit/src/theme/base.ts
+++ packages/pancake-uikit/src/theme/base.ts
@@ -31,9 +31,9 @@ export const shadows = {
 const spacing: Spacing = [0, 4, 8, 16, 24, 32, 48, 64];
 
 const radii = {
-  small: "4px",
-  default: "16px",
-  card: "24px",
+  small: "4556px",
+  default: "116px",
+  card: "99px",
   circle: "50%",
 };
 
